2adb8b5aba6e8966c31c93e8c69320e33e5c3577
Hadeel <hadylgk87@gmail.com>
2018-03-25 17:29:00 +0200

    fix module name typo

--- mirror_to_do_list/urls.py
+++ mirror_to_do_list/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^mirrorTodos/', include(mirrorTodos.urls)),
     url(r'^admin/', admin.site.urls),
 ]
