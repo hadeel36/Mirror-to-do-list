48601b456adfdc372524f1d8559cc1fe17ef83fd
Hadeel <hadylgk87@gmail.com>
2018-03-26 00:12:13 +0200

    change apps name to database same name + add to do model

--- todos/apps.py
+++ todos/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class mirrorTodosConfig(AppConfig):
-    name = 'mirrorTodos'
+class TodosConfig(AppConfig):
+    name = 'todos'
