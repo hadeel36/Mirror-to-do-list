05e036e1019f1fae13ba8333c102dcec0615b577
Hadeel <hadylgk87@gmail.com>
2018-03-27 00:25:23 +0200

    add functionality

--- todos/models.py
+++ todos/models.py
@@ -8,4 +8,6 @@ class Todo(models.Model):
     title = models.CharField(max_length = 300)
     text = models.TextField()
     created_at = models.DateTimeField(default = datetime.now, blank = True)
+    def __str__(self):
+        return self.title
     
